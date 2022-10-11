386c73a15c09a55d07235e4c9fa66ec98719a264
product-validation96 <gohelgohel86@gmail.com>
2022-10-11 16:27:48 +0530

    product

---
+++ project1.01.php
@@ -0,0 +1,34 @@
+<html>
+<head><link rel="stylesheet" type="text/css" href="style4.css"></head>
+<body>
+
+<div class="sign-up-form">
+<img src="icon.png.png" class="usericon">
+<h1>Login</h1>
+
+<FORM action="connect3.php" method="post">
+
+<b>Email id:</b>
+<input type="email" class="input-box" id="email" name="email" placeholder="Enter Your Email" required><br><br>
+
+<b>Password:</b>
+<input type="password" class="input-box" id="password" name="password" placeholder="Enter Password" required><br><br>
+
+<b>Name:</b>
+<input class="input-box" type="text" name="Name" placeholder="Enter Name" required><br><br>
+
+<b><span><input class="checkbox" type="checkbox" required></span>I agree to the terms of services</b>
+
+<button class="signup-btn" type="submit" value="Submit">Submit</button><br><br>
+ 
+<button type="reset" class="signup-btn" name="reset" value="Reset">Reset</button><br><br>
+
+<hr>
+<b><p class="or">OR</p></b>
+
+<a href="project1.2.php"><h3>Sign Up</h3></a>
+<a href="project.html"><h3>Home</h3></a>
+</FORM>	
+</div>
+</body>
+</html>
---
+++ project1.2.php
@@ -0,0 +1,41 @@
+<html>
+<head><link rel="stylesheet" type="text/css" href="style4.css"></head>
+<body>
+
+<div class="sign-up-form">
+<img src="icon.png.png" class="usericon">
+<h1>Sign Up</h1>
+
+<FORM action="connect.php" method="post">
+
+<b>Email id:</b>
+<input class="input-box" type="email" name="email" placeholder="Enter Email" required><br><br>
+
+<b>Password:</b>
+<input class="input-box" type="password" name="password" placeholder="Enter Password" required><br><br>
+
+<b>Mobile:</b>
+<input class="input-box" type="number" name="number" placeholder="Enter Mobile" required><br><br>
+
+<b>City Name:</b>
+<input class="input-box" type="text" id="City" name="City" placeholder="Enter City Name" required><br><br>
+
+<b>Name:</b>
+<input class="input-box" type="text" name="Name" placeholder="Enter Name" required><br><br>
+
+<b><span><input class="checkbox" type="checkbox" required></span>I agree to the terms of services</b>
+
+<button type="submit" class="signup-btn" value="Submit">Submit</button><br><br>
+
+<button type="reset" class="signup-btn" name="reset" value="Reset">Reset</button><br><br>
+
+<hr>
+<b><p class="or">OR</p></b>
+
+<b>Already Have Account?</b><a href="project1.01.php"><h3>Login</h3></a>
+<a href="project.html"><h3>Home</h3></a>
+
+</FORM>
+</div>
+</body>
+</html>
